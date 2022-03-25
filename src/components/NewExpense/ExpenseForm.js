97f551b7cd86eb2f2628bb6cde3adb3cf15fbe34
Gagan Ichake <gaganichake@yahoo.com>
2022-03-25 13:22:41 -0500

    ExpenseForm (child) to NewExpense (parent) component comminucation

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,7 +1,7 @@
 import "./ExpenseForm.css"
 import { useState } from "react";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
 
     const [enteredTitle, setEnteredTitle] = useState("");
     const [enteredAmount, setEnteredAmount] = useState("");
@@ -78,7 +78,7 @@ const ExpenseForm = () => {
             date: new Date(enteredDate)
         };
 
-        console.log(expenseData);
+        props.onSaveExpenseData(expenseData);
         setEnteredTitle('');
         setEnteredAmount('');
         setEnteredDate('');
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -3,11 +3,20 @@ import ExpenseForm from "./ExpenseForm";
 
 const NewExpense = () => {
 
+    const onSaveExpenseDataHandler = (enteredExpenseData) => {
+        
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString()
+        }
+        console.log(expenseData);
+    };
+
     return (
         <div className="new-expense">
-            <ExpenseForm/>
+            <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler}/>
         </div>
     );
-}
+};
 
 export default NewExpense;
