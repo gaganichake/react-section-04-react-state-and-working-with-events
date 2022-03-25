50b09105ac6624d498f853d9fd92bedc70b6c337
Gagan Ichake <gaganichake@yahoo.com>
2022-03-25 15:01:28 -0500

    Assignment 2 - Working with Events & State

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,22 @@
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
 import Card from "./../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import { useState } from 'react';
 
 const Expenses = (props) => {
   const expenses = props.expenses;
 
+  const [filteredYear, setFilteredYear] = useState('2022');// You may set the default year here
+
+  const filterYearHander = (selectedYear) => {
+    setFilteredYear(selectedYear);
+    //console.log(selectedYear);
+  }
+
   return (
     <Card className="expenses">
+      <ExpensesFilter defaultYear={filteredYear} onFilterYear={filterYearHander}/>
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
